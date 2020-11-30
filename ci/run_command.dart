d92db1172597fb97f8c12abbf8c5d040bf1840ec
Serban Blebea <serban.blebea4@gmail.com>
2020-11-30 21:29:55 +0200

    final format no warnings

--- ci/run_command.dart
+++ ci/run_command.dart
@@ -84,7 +84,7 @@ Stream<String> runAndGetStdout(
   unawaited(stderr.addStream(process.stderr));
   final Stream<String> lines =
       process.stdout.transform(utf8.decoder).transform(const LineSplitter());
-  await for (String line in lines) {
+  await for (final String line in lines) {
     yield line;
   }
 
